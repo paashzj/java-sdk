80dd7f6d753b6d3e0a6e46b6f6879c67af183128
goflutterjava <95280282+goflutterjava@users.noreply.github.com>
2021-12-28 17:52:19 +0800

    add test in random util (#3)

---
+++ java-tool/src/test/java/com/github/shoothzj/javatool/util/RandomUtilTest.java
@@ -0,0 +1,32 @@
+package com.github.shoothzj.javatool.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class RandomUtilTest {
+
+    @Test
+    public void textRandomBoolean() {
+        RandomUtil.randomBoolean();
+    }
+
+    @Test
+    public void textRandomInt() {
+        int nextInt = RandomUtil.nextInt();
+        Assertions.assertTrue(nextInt >= 0);
+    }
+
+    @Test
+    public void textRandomIntWithBound() {
+        int nextInt = RandomUtil.nextInt(5);
+        Assertions.assertTrue(nextInt < 5);
+    }
+
+    @Test
+    public void textRandomIntRange() {
+        int nextInt = RandomUtil.randomInt(3, 5);
+        Assertions.assertTrue(nextInt < 5);
+        Assertions.assertTrue(nextInt >= 3);
+    }
+
+}
